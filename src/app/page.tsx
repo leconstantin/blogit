d6f731b9ea1eb25f11224e95971b3d4db4e08975
leconstantin <lcon69184@gmail.com>
2025-01-15 00:27:08 +0200

    added some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import {
-  RegisterLink,
-  LoginLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
-
 export default function Home() {
   return (
-    <div>
-      <LoginLink>Sign in</LoginLink>
-      <RegisterLink>Sign up</RegisterLink>
-    </div>
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl font-bold mb-5">Welcome to my blog</h1>
+      <p className="max-w-[750px] mx-auto leading-8">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
+        excepturi odit fugit adipisci ipsum ut necessitatibus. Sint voluptates
+        dicta hic, dignissimos nesciunt necessitatibus tempore at praesentium
+        soluta quas animi nulla!
+      </p>
+    </main>
   );
 }
