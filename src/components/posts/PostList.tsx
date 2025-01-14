1bd9fcb6f81ef4fc88628c2017b8ab4cf0739af8
leconstantin <lcon69184@gmail.com>
2025-01-15 00:52:22 +0200

    added dummy data

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -1,12 +1,13 @@
-import { prisma } from "@/lib/db";
 import Link from "next/link";
 
 export default async function PostsList() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const posts = await prisma.post.findMany();
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  const posts = (await response.json()).posts;
+
   return (
     <ul>
-      {posts.map((post) => (
+      {posts.map((post: { id: number; title: string }) => (
         <li key={post.id} className="mb-3">
           <Link href={`/posts/${post.id}`}>{post.title}</Link>
         </li>
